fb8c2b79f8e4a2740eb442cc1007aee1a990bd20
BooSung Jung <jungboosung01@gmail.com>
2024-09-03 11:49:01 +1000

    Initial commit

---
+++ SimpleGlobe/Views/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SimpleGlobe
+//
+//  Created by Bernhard Jenny on 14/8/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    @Environment(ViewModel.self) var model
+
+    var body: some View {
+        VStack {
+            GlobeButton(globe: model.globe)
+                .padding()
+        }
+        .padding(50)
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(ViewModel.preview)
+}
